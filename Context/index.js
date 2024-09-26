88b09b7f687c523618c3a822d2338b86ec9c2b9f
Thebinary110 <shivaanroy614@gmail.com>
2024-09-27 01:46:22 +0530

    production ready deploy

--- Context/index.js
+++ Context/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, createContext, useEffect } from "react";
 import { ethers } from "ethers";
 
-//INTERNAL IMPORT
+// INTERNAL IMPORT
 import {
   ChechIfWalletConnected,
   connectWallet,
@@ -11,21 +11,21 @@ import {
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  //  STATE VARIABLE
+  // STATE VARIABLES
   const [address, setAddress] = useState("");
   const [contractMembership, setContractMembership] = useState([]);
   const [Free, setFree] = useState();
   const [userMembership, setUserMembership] = useState({});
 
-  //FETCH DATA TIME OF PAGE LOAD
+  // FETCH DATA ON PAGE LOAD
   const fetchData = async () => {
     try {
-      //LOAD FREE TRAIL
+      // LOAD FREE TRIAL
       const freeTrail = localStorage.getItem("freeTrail");
       const FREE_TRAIL = JSON.parse(freeTrail);
       setFree(freeTrail);
 
-      //GET CONTRACT
+      // GET CONTRACT
       const contract = await connectingWithContract();
       const connectAccount = await connectWallet();
       setAddress(connectAccount);
@@ -36,8 +36,8 @@ export const StateContextProvider = ({ children }) => {
       const sixMonth = await contract.getMembership(2);
       const oneYear = await contract.getMembership(3);
 
-      //LIST OF MEMBERSHIPS
-      contractMembership = [
+      // LIST OF MEMBERSHIPS
+      setContractMembership([
         {
           memberShip_name: oneMonth?.name,
           memberShip_date: oneMonth?.date,
@@ -65,38 +65,41 @@ export const StateContextProvider = ({ children }) => {
             "ether"
           ),
         },
-      ];
-      setContractMembership(contractMembership);
+      ]);
 
-      //USER MEMBERSHIP
-      const userMembership = await contract.getUserMembership(connectAccount);
+      // USER MEMBERSHIP
+      const userMembershipData = await contract.getUserMembership(connectAccount);
 
-      userMembership = {
-        addressUser: userMembership.addressUser.toLowerCase(),
-        expireDate: userMembership.expireDate,
-        cost: ethers.utils.formatUnits(userMembership.cost.toString(), "ether"),
-        membershipId: userMembership.membershipId.toNumber(),
-        id: userMembership.id.toNumber(),
-      };
-      setUserMembership(userMembership);
+      setUserMembership({
+        addressUser: userMembershipData.addressUser.toLowerCase(),
+        expireDate: userMembershipData.expireDate,
+        cost: ethers.utils.formatUnits(
+          userMembershipData.cost.toString(),
+          "ether"
+        ),
+        membershipId: userMembershipData.membershipId.toNumber(),
+        id: userMembershipData.id.toNumber(),
+      });
 
-      const proMember = JSON.stringify(userMembership);
+      const proMember = JSON.stringify(userMembershipData);
       localStorage.setItem("UserDetail", proMember);
     } catch (error) {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const listMembership = async () => {
-    //LISTING MEMBERSHIP
+    // LISTING MEMBERSHIP
     const amount = 5;
     const MEMBERSHIP_NAME = "One Year";
     const MEMBERSHIP_COST = ethers.utils.parseUnits(amount.toString(), "ether");
     const MEMBERSHIP_DATE = "July 31 2023";
-    //CONTRACT
+
+    // CONTRACT
     const contract = await connectingWithContract();
     const list = await contract.list(
       MEMBERSHIP_NAME,
@@ -113,7 +116,7 @@ export const StateContextProvider = ({ children }) => {
     const connectAccount = await connectWallet();
     setAddress(connectAccount);
     try {
-      if (memberShip_id == 1) {
+      if (memberShip_id === 1) {
         const today = Date.now() + 2657228546;
         let date = new Date(today);
         const expiredDate = date.toLocaleDateString("en-US");
@@ -130,14 +133,14 @@ export const StateContextProvider = ({ children }) => {
         await mintTransaction.wait();
         const freeTrail = JSON.stringify("Pro Member");
         localStorage.setItem("freeTrail", freeTrail);
-        console.info("contract call successs", mintTransaction);
+        console.info("contract call success", mintTransaction);
         window.location.reload();
-      } else if (memberShip_id == 2) {
+      } else if (memberShip_id === 2) {
         const today = Date.now() + 2657228546 * 6;
         let date = new Date(today);
         const expiredDate = date.toLocaleDateString("en-US");
-        console.log(expiredDate);
         const money = ethers.utils.parseEther("3");
+
         const mintTransaction = await contract.mint(
           memberShip_id,
           connectAccount,
@@ -149,14 +152,14 @@ export const StateContextProvider = ({ children }) => {
         await mintTransaction.wait();
         const freeTrail = JSON.stringify("Pro Member");
         localStorage.setItem("freeTrail", freeTrail);
-        console.info("contract call successs", mintTransaction);
+        console.info("contract call success", mintTransaction);
         window.location.reload();
       } else {
         const today = Date.now() + 31601495874;
         let date = new Date(today);
         const expiredDate = date.toLocaleDateString("en-US");
-        console.log(expiredDate);
         const money = ethers.utils.parseEther("5");
+
         const mintTransaction = await contract.mint(
           memberShip_id,
           connectAccount,
@@ -166,10 +169,9 @@ export const StateContextProvider = ({ children }) => {
           }
         );
         await mintTransaction.wait();
-        const pro_member = "Pro Member";
-        const freeTrail = JSON.stringify(pro_member);
+        const freeTrail = JSON.stringify("Pro Member");
         localStorage.setItem("freeTrail", freeTrail);
-        console.info("contract call successs", mintTransaction);
+        console.info("contract call success", mintTransaction);
         window.location.reload();
       }
     } catch (error) {
